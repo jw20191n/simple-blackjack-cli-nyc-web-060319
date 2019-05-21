d1444be574b38d5c4edac7e26afdbc9cb9db6e05
Jiayi Wang <clairejw1015@gmail.com>
2019-05-21 23:14:01 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -65,8 +65,8 @@ def runner
   # code runner here
   welcome
   card_total = initial_round
-  hit? 
-  display_card_total
+  hit? (card_total)
+  display_card_total(card_total)
   
   until card_total > 21
     end_game
